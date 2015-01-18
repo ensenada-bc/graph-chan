f50e81382c3b29f567b4ea708a2451497ddf4832
Graeme Wolfendale <gwolfendale@gmail.com>
2015-01-18 04:28:51 +0000

    Update graph.js

--- graph.js
+++ graph.js
@@ -1,1 +1,41 @@
 //code to display a graph using the translated json and cytoscape.js
+//setup a basic cytoscape graph
+var cy = cytoscape({
+  // very commonly used options:
+  container: undefined, 
+  elements: [ /* ... */ ],
+  style: [ /* ... */ ],
+  layout: { name: 'grid' /* , ... */ },
+  ready: function(evt){ /* ... */ },
+
+  // initial viewport state:
+  zoom: 1,
+  pan: { x: 0, y: 0 },
+
+  // interaction options:
+  minZoom: 1e-50,
+  maxZoom: 1e50,
+  zoomingEnabled: true,
+  userZoomingEnabled: true,
+  panningEnabled: true,
+  userPanningEnabled: true,
+  boxSelectionEnabled: false,
+  selectionType: (isTouchDevice ? 'additive' : 'single'),
+  touchTapThreshold: 8,
+  desktopTapThreshold: 4,
+  autolock: false,
+  autoungrabify: false,
+  autounselectify: false,
+
+  // rendering options:
+  headless: false,
+  styleEnabled: true,
+  hideEdgesOnViewport: false,
+  hideLabelsOnViewport: false,
+  textureOnViewport: false,
+  motionBlur: false,
+  wheelSensitivity: 1,
+  pixelRatio: 1,
+  initrender: function(evt){ /* ... */ },
+  renderer: { /* ... */ }
+});
