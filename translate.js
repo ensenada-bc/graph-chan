6612d17bf8007e597db5b859e1ee098c37f1aded
Graeme Wolfendale <gwolfendale@gmail.com>
2015-01-19 06:21:28 +0000

    Update translate.js

--- translate.js
+++ translate.js
@@ -1,1 +1,7 @@
 //code to translate the imageboard api response json into json that cytoscape will accept
+function translate_json(raw_json)
+{
+  //extract the content from each post into a nodes list
+  //extract the links between nodes into a links list
+  //return the links list
+}
